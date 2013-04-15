e7add93a5b2edddb18133ebadb07ec7ee8b372d3
Sean Connolly <connolly.st@gmail.com>
2013-04-15 22:32:28 +0200

    Added a 'List..' button on the main activity to take you to the Card List.. just to get us going.

---
+++ src/main/java/org/dosbcn/percolator/events/GotoListButtonClickListener.java
@@ -0,0 +1,18 @@
+package org.dosbcn.percolator.events;
+
+import android.view.View;
+
+public class GotoListButtonClickListener implements View.OnClickListener {
+
+    /**
+     * Handle a click event.
+     *
+     * @param view
+     *         the view originating the click event
+     */
+    @Override
+    public void onClick(View view) {
+
+    }
+
+}
