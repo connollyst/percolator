8629867e2c2a290d8f7c9aad1b47cc85d03f7e58
Sean Connolly <connolly.st@gmail.com>
2013-11-23 18:09:53 +0100

    typo

--- src/main/java/org/dosbcn/percolator/MainActivity.java
+++ src/main/java/org/dosbcn/percolator/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
         // Make sure an alarm is queued for all active cards, the queue is smart
         // enough to keep appropriate timing and avoid duplicates etc.
         service.resetAllAlarms();
-        // TODO do this when the application starts.. now the activity
+        // TODO do this when the application starts.. not the activity
         // TODO do this in another thread so the application starts quickly
         // http://developer.android.com/guide/components/processes-and-threads.html
     }
