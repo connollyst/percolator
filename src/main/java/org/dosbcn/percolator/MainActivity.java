5e1c669b0d3d3345c7ab811bde5e56aeb2b952d7
Sean Connolly <connolly.st@gmail.com>
2013-11-24 15:39:50 +0100

    Added menu.

--- src/main/java/org/dosbcn/percolator/MainActivity.java
+++ src/main/java/org/dosbcn/percolator/MainActivity.java
@@ -3,6 +3,9 @@ package org.dosbcn.percolator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,62 +23,89 @@ import org.dosbcn.percolator.events.SaveButtonClickListener;
  */
 public class MainActivity extends Activity {
 
-    private final CardService service;
+	private final CardService service;
 
-    public MainActivity() {
-        service = new CardServiceImpl(this);
-    }
+	public MainActivity() {
+		service = new CardServiceImpl(this);
+	}
 
-    protected MainActivity(CardService service) {
-        this.service = service;
-    }
+	protected MainActivity(CardService service) {
+		this.service = service;
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        initSaveButtonListener();
-        initGotoListButtonListener();
-        // Make sure an alarm is queued for all active cards, the queue is smart
-        // enough to keep appropriate timing and avoid duplicates etc.
-        service.resetAllAlarms();
-        // TODO do this when the application starts.. not the activity
-        // TODO do this in another thread so the application starts quickly
-        // http://developer.android.com/guide/components/processes-and-threads.html
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		initSaveButtonListener();
+		// Make sure an alarm is queued for all active cards, the queue is smart
+		// enough to keep appropriate timing and avoid duplicates etc.
+		service.resetAllAlarms();
+		// TODO do this when the application starts.. not the activity
+		// TODO do this in another thread so the application starts quickly
+		// http://developer.android.com/guide/components/processes-and-threads.html
+	}
 
-    private void initSaveButtonListener() {
-        findSaveButton().setOnClickListener(new SaveButtonClickListener(this));
-    }
+	private void initSaveButtonListener() {
+		findSaveButton().setOnClickListener(new SaveButtonClickListener(this));
+	}
 
-    private void initGotoListButtonListener() {
-        findGotoListButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent nextActivity = new Intent(getApplicationContext(), ListActivity.class);
-                startActivity(nextActivity);
-            }
-        });
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    public CardService getService() {
-        return service;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_list:
+			openList();
+			return true;
+		case R.id.action_settings:
+			openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    public EditText findTitleField() {
-        return (EditText) findViewById(R.id.title_input);
-    }
+	public void openList() {
+		Intent nextActivity = new Intent(getApplicationContext(),
+				ListActivity.class);
+		startActivity(nextActivity);
+	}
 
-    public EditText findDescriptionField() {
-        return (EditText) findViewById(R.id.description_input);
-    }
+	public void openSettings() {
 
-    public Button findSaveButton() {
-        return (Button) findViewById(R.id.save_button);
-    }
+	}
 
-    public Button findGotoListButton() {
-        return (Button) findViewById(R.id.list_button);
-    }
+	public CardService getService() {
+		return service;
+	}
+
+	public EditText findTitleField() {
+		return (EditText) findViewById(R.id.title_input);
+	}
+
+	public EditText findDescriptionField() {
+		return (EditText) findViewById(R.id.description_input);
+	}
+
+	public Button findSaveButton() {
+		return (Button) findViewById(R.id.save_button);
+	}
 
 }
